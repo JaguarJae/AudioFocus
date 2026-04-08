357ae18d4f30134be158802e26bc54349f63fb1c
JaguarJAE <Jaiarjest@gmail.com>
2026-04-08 22:05:51 +0200

    Intial Setup

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Windows.Media.Control;
+
+namespace AudioFocus
+{
+    class AudioFocus
+    {
+        static async Task Main(string[] args)
+        {
+            
+            async Task Sessions()
+            {
+                var manager = await GlobalSystemMediaTransportControlsSessionManager.RequestAsync();
+                var sessions = manager.GetSessions();
+
+                foreach (var session in sessions)
+                {
+                    Console.WriteLine(session.SourceAppUserModelId);
+                }
+            }
+
+            await Sessions();
+
+            Console.ReadLine();
+        }
+    }
+}
